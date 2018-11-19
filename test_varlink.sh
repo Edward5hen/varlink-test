2275a59661c6a22b53157b83a766cdfa7dff3226
Edward Shen <weshen@redhat.com>
2018-11-19 05:26:43 -0500

    Add some test steps

--- test_varlink.sh
+++ test_varlink.sh
@@ -14,91 +14,87 @@
 #     4. Pause container. -->> varlink method: PauseContainer()
 #     5. Unpause container. -->> varlink method: UnpauseContainer()
 #     6. Stop container. -->> varlink method: StopContainer()
-#     7. Start container. -->> varlink method: StartContainer()
-#     8. Remove container. -->> valink method: RemoveContainer()
+#     7. Remove container. -->> valink method: RemoveContainer()
 # ---------------------------------------------------------------
 
-# ------------------------ main --------------------------------
-#set -e
-
+# ------------------------ Global Variables -------------------------
 CMD_PREFIX="varlink call unix:/run/podman/io.podman/io.podman."
 
-#echo "Setup: Remove all containers and images."
-#podman rm -af
-#podman rmi -af
-#
-#echo "Step1: pull image alpine and check"
-#pull_result=`${CMD_PREFIX}PullImage '{"name": "alpine"}'`
-#if [ "$?" -ne 0 ]; then
-#    echo "Pull image alpine failed!!!"
-#fi
-#echo "${pull_result}"
-#image_id=`echo ${pull_result} | grep -E '[[:alnum:]]{64}'`
-#if [ "$?" -ne 0 ]; then
-#    echo "Image ID is not printed in output!!!"
-#fi
+# ------------------------ Functions --------------------------------
+verify() {
+    if [ "$?" -ne 0 ]; then
+        echo "FAIL: $1 failed!!!"
+    else
+        echo "PASS: $1 passed."
+    fi
+}
 
-image_id="196d12cf6ab19273823e700516e98eb1910b03b17840f9d5509f03858484d321"
+# ------------------------   main  ----------------------------------
+
+echo "Setup: Remove all containers and images."
+podman rm -af
+podman rmi -af
+
+echo "Step1: pull image alpine and check"
+pull_result=`${CMD_PREFIX}PullImage '{"name": "alpine"}'`
+verify "Pull image with PullImage()"
+echo "${pull_result}"
+image_id=`echo ${pull_result} | grep -E '[[:alnum:]]{64}'`
+verify "Image ID print"
 
 echo
 echo "Step2-1: check image with ListImages()"
 list_result=`${CMD_PREFIX}ListImages`
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: List images failed!!!"
-else
-    echo "PASS"
-fi
+verify "List image with ListImages()"
 echo ${list_result} | grep "${image_id}"
-if [ "$?" -ne 0 ]; then
-    echo "Output of ListImages() is Wrong!!!"
-else
-    echo "PASS"
-fi
+verify "Image id print"
 
 echo
 echo "Step2-2: check image with GetImage()"
 get_img_result=`${CMD_PREFIX}GetImage '{"name": "docker.io/library/alpine:latest"}'`
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Get Image failed!!!"
-else
-    echo "PASS"
-fi
+verify "Get image with GetImage()"
 echo ${get_img_result} | grep "${image_id}"
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Output of GetImage() is Wrong!!!"
-else
-    echo "PASS"
-fi
+verify "Image ID print"
 
 echo
 echo "Step3-1: create container with image alpine and check"
 create_result=`${CMD_PREFIX}CreateContainer '{"create": {"image": "alpine", "name": "test", "command": ["/usr/bin/top"], "detach": true}}'`
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Create container failed!!!"
-else
-    echo "PASS"
-fi
+verify "Create container test with CreateContainer()"
 echo "${create_result}"
 container_id=`echo ${create_result} | grep -E '[[:alnum:]]{64}'`
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Container ID is not printed in output!!!"
-else
-    echo "PASS"
-fi
+verify "Container ID print"
 
 echo
 echo "Step3-2: start container test"
 start_result=`${CMD_PREFIX}StartContainer '{"name": "test"}'`
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Start container failed!!!"
-else
-    echo "PASS"
-fi
+verify "Start container test with StartContainer()"
 echo ${start_result} | grep "${container_id}"
-if [ "$?" -ne 0 ]; then
-    echo "FAIL: Container id is not printed in output!!!"
-else
-    echo "PASS"
-fi
+verify "Container ID print"
 
+echo
+echo "Step-4: pause container test"
+pause_result=`${CMD_PREFIX}PauseContainer '{"name": "test"}'`
+verify "Pause container test with PauseContainer()"
+echo ${pause_result} | grep "${container_id}"
+verify "Container id print"
 
+echo
+echo "Step-5: unpause container test"
+unpause_result=`${CMD_PREFIX}UnpauseContainer '{"name": "test"}'`
+verify "Unpause container test with UnpauseContainer()"
+echo ${unpause_result} | grep "${container_id}"
+verify "Container ID print"
+
+echo
+echo "Step-6: stop container test with timeout 5s"
+stop_result=`${CMD_PREFIX}StopContainer '{"name": "test", "timeout": 5}'`
+verify "Stop container test with StopContainer()"
+echo ${stop_result} | grep "${container_id}"
+verify "Container ID print"
+
+echo
+echo "Step-7: remove container test"
+rmv_result=`${CMD_PREFIX}RemoveContainer '{"name": "test"}'`
+verify "Remove container test with RemoveContainer()"
+echo ${rmv_result} | grep "${container_id}"
+verify "Container ID print"
